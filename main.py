ee54abc8bb9c9408a0a8c9f89cec492ff37c86e1
Kevin Pan <pankevin2276262271@gmail.com>
2024-08-09 15:42:18 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 import torch
-import pandas as pd
-import random
-import cv2
-import os
-import sys
-import numpy as np
-from PIL import Image 
-import albumentations as A 
-from albumentations.pytorch import ToTensorV2 
-from torch.utils.data import DataLoader, random_split
+from torch.utils.data import DataLoader
+import pickle
+from torch.cuda.amp import autocast
 
-from model_YOLOv3 import convert_cells_to_bboxes, nms, iou, YOLOv3, YOLOLoss
-from visualization import plot_image
-from training import model_training, feedforward
+from yolo_v8 import YOLO, non_max_suppression, xy2wh
+from dataset import YOLO_Dataset
+from visualization import plot_image, plot_images
+from training import model_training 
 
-# supports MacOS mps and CUDA
-def GPU_Device():
+
+
+def compute_device():
     if torch.cuda.is_available():
         return 'cuda'
     elif torch.backends.mps.is_available():
@@ -24,304 +19,159 @@ def GPU_Device():
         return 'cpu'
     
     
-# Class labels 
-class_labels = [ 
-    "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", 
-    "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", 
-    "pottedplant", "sheep", "sofa", "train", "tvmonitor"
-]
 
-
-# Image size 
-image_size = 416
+# bidirectional dictionary
+class BidirectionalMap:
+    def __init__(self):
+        self.key_to_value = {}
+        self.value_to_key = {}
     
-# Anchor boxes for each feature map scaled between 0 and 1 
-# 3 feature maps at 3 different scales based on YOLOv3 paper 
-ANCHORS = [ 
-    [(0.28, 0.22), (0.38, 0.48), (0.9, 0.78)], 
-    [(0.07, 0.15), (0.15, 0.11), (0.14, 0.29)], 
-    [(0.02, 0.03), (0.04, 0.07), (0.08, 0.06)], 
-] 
+    def __len__(self):
+        return len(self.key_to_value)
+    
+    def add_mapping(self, key, value):
+        self.key_to_value[key] = value
+        self.value_to_key[value] = key
 
-# Grid cell sizes 
-grid_cell_sizes = [image_size // 32, image_size // 16, image_size // 8] 
+    def get_value(self, key):
+        return self.key_to_value.get(key)
 
-# Defining the grid size and the scaled anchors 
-scaled_anchors = (torch.tensor(ANCHORS) * 
-    torch.tensor(grid_cell_sizes).unsqueeze(1).unsqueeze(1).repeat(1,3,2)).to(GPU_Device()) 
+    def get_key(self, value):
+        return self.value_to_key.get(value)
 
-
-# augmumentation transform
-aug_transforms = A.Compose(
-    [
-        A.LongestMaxSize(max_size=image_size),
-        # Pad remaining areas with zeros 
-        A.PadIfNeeded( 
-            min_height=image_size, min_width=image_size, border_mode=cv2.BORDER_CONSTANT 
-        ), 
-        A.ColorJitter(p=0.4),
-        A.HorizontalFlip(p=0.5),
-        A.Blur(p=0.1),
-        A.CLAHE(p=0.1),
-        A.Posterize(p=0.1),
-        A.ToGray(p=0.1),
-        A.ChannelShuffle(p=0.05),
-        A.Normalize(mean=[0, 0, 0], std=[1, 1, 1], max_pixel_value=255,),
-        ToTensorV2(),
-    ],
-    bbox_params=A.BboxParams(format="yolo", min_visibility=0.4, label_fields=[],),
-)
-
-
-# Transform for test and inference 
-resize_transform = A.Compose( 
-    [ 
-        # Rescale an image so that maximum side is equal to image_size 
-        A.LongestMaxSize(max_size=image_size), 
-        # Pad remaining areas with zeros 
-        A.PadIfNeeded( 
-            min_height=image_size, min_width=image_size, border_mode=cv2.BORDER_CONSTANT 
-        ), 
-        # Normalize the image 
-        A.Normalize(mean=[0, 0, 0], std=[1, 1, 1], max_pixel_value=255), 
-        # Convert the image to PyTorch tensor 
-        ToTensorV2() 
-    ],  
-    # Augmentation for bounding boxes 
-    bbox_params=A.BboxParams( format="yolo", min_visibility=0.4, label_fields=[] ) 
-) 
 
 
 
 # model inference
-def inference(img, model):
+@torch.no_grad
+def inference(model, img):
     model.eval()
-    with torch.no_grad():
-        # unsqueeze the batch dim
-        img = img.unsqueeze(0)
-        
-        # Getting the model predictions 
-        output = model(img) 
-
-        # Getting the boxes coordinates from the labels 
-        # and converting them into bounding boxes without scaling 
-        boxes = [] 
-        for j in range(output[0].shape[1]): # 3 scales
-            boxes += convert_cells_to_bboxes(output[j], scaled_anchors[j])[0]
-        
-        # Applying non-maximum suppression 
-        boxes = nms(boxes, iou_threshold=0.45, confidence_threshold=0.6) 
-          
-    return boxes
-
-
-
-# Create a dataset class to load the images and labels from the folder 
-class Dataset(torch.utils.data.Dataset): 
-    def __init__( 
-        self, csv_file, image_dir, label_dir, anchors, image_size=416, 
-        grid_sizes=[13, 26, 52], num_classes=20, transform=None ): 
-        # Read the csv file with image names and labels 
-        self.label_list = pd.read_csv(csv_file) 
-        # Image and label directories 
-        self.image_dir = image_dir 
-        self.label_dir = label_dir 
-        # Image size 
-        self.image_size = image_size 
-        # Transformations 
-        self.transform = transform 
-        # Grid sizes for each scale 
-        self.grid_sizes = grid_sizes 
-        # Anchor boxes 
-        self.anchors = torch.tensor(anchors[0] + anchors[1] + anchors[2]) 
-        # Number of anchor boxes  
-        self.num_anchors = self.anchors.shape[0] 
-        # Number of anchor boxes per scale 
-        self.num_anchors_per_scale = self.num_anchors // 3
-        # Number of classes 
-        self.num_classes = num_classes 
-        # Ignore IoU threshold 
-        self.ignore_iou_thresh = 0.5
-  
-    def __len__(self): 
-        return len(self.label_list) 
     
+    # move to device
+    img = img.to(compute_device())
     
-    def __getitem__(self, idx):
-        # Getting the label path 
-        label_path = os.path.join(self.label_dir, self.label_list.iloc[idx, 1]) 
-        # We are applying roll to move class label to the last column 
-        # 5 columns: x, y, width, height, class_label 
-        bboxes = np.roll(np.loadtxt(fname=label_path, 
-                         delimiter=" ", ndmin=2), 4, axis=1).tolist() 
-          
-        # Getting the image path 
-        img_path = os.path.join(self.image_dir, self.label_list.iloc[idx, 0]) 
-        image = np.array(Image.open(img_path).convert("RGB"))
-        
-        # Albumentations augmentations 
-        if self.transform: 
-            augs = self.transform(image=image, bboxes=bboxes) 
-            image = augs["image"] 
-            bboxes = augs["bboxes"] 
-        
-        # Below assumes 3 scale predictions (as paper) and same num of anchors per scale 
-        # target : [probabilities, x, y, width, height, class_label] 
-        targets = [torch.zeros((self.num_anchors_per_scale, s, s, 6)) 
-                   for s in self.grid_sizes] 
-          
-        # Identify anchor box and cell for each bounding box 
-        for box in bboxes: 
-            # Calculate iou of bounding box with anchor boxes 
-            iou_anchors = iou(torch.tensor(box[2:4]), self.anchors, is_pred=False) 
-            # Selecting the best anchor box 
-            anchor_indices = iou_anchors.argsort(descending=True, dim=0) 
-            x, y, width, height, class_label = box 
-  
-            # At each scale, assigning the bounding box to the  
-            # best matching anchor box 
-            has_anchor = [False] * 3
-            for anchor_idx in anchor_indices: 
-                scale_idx = anchor_idx // self.num_anchors_per_scale 
-                anchor_on_scale = anchor_idx % self.num_anchors_per_scale 
-                  
-                # Identifying the grid size for the scale 
-                s = self.grid_sizes[scale_idx] 
-                  
-                # Identifying the cell to which the bounding box belongs 
-                i, j = int(s * y), int(s * x) 
-                anchor_taken = targets[scale_idx][anchor_on_scale, i, j, 0] 
-                  
-                # Check if the anchor box is already assigned 
-                if not anchor_taken and not has_anchor[scale_idx]: 
-  
-                    # Set the probability to 1 
-                    targets[scale_idx][anchor_on_scale, i, j, 0] = 1
-  
-                    # Calculating the center of the bounding box relative to the cell 
-                    x_cell, y_cell = s * x - j, s * y - i  
-  
-                    # Calculating the width and height of the bounding box  
-                    # relative to the cell 
-                    width_cell, height_cell = (width * s, height * s) 
-  
-                    # Idnetify the box coordinates 
-                    box_coordinates = torch.tensor([x_cell, y_cell, width_cell, height_cell] ) 
-  
-                    # Assigning the box coordinates to the target 
-                    targets[scale_idx][anchor_on_scale, i, j, 1:5] = box_coordinates 
-  
-                    # Assigning the class label to the target 
-                    targets[scale_idx][anchor_on_scale, i, j, 5] = int(class_label) 
-  
-                    # Set the anchor box as assigned for the scale 
-                    has_anchor[scale_idx] = True
-  
-                # If the anchor box is already assigned, check if the  
-                # IoU is greater than the threshold 
-                elif not anchor_taken and iou_anchors[anchor_idx] > self.ignore_iou_thresh: 
-                    # Set the probability to -1 to ignore the anchor box 
-                    targets[scale_idx][anchor_on_scale, i, j, 0] = -1
-        
-        # Return the image and the target 
-        return image, tuple(targets)
+    # unsqueeze batch dimension
+    img = img.unsqueeze(0)
     
+    with autocast(dtype=torch.float16):
+        _, box = model(img)
 
-
-
+    # apply nms
+    # pred shape -> batch_size * torch.Size([(num_bbox, 6)])
+    pred = non_max_suppression(box, 0.2, 0.65)[0]
+    # convert from xyxy to xywh
+    pred = pred.cpu().numpy()
+    pred = xy2wh(pred)
+    
+    # normalize to [0, 1]
+    # inplace clip
+    _, _, w, h = img.shape
+    pred[:, [0, 2]] = pred[:, [0, 2]].clip(0, w - 1E-3) / w
+    pred[:, [1, 3]] = pred[:, [1, 3]].clip(0, h - 1E-3) / h
+    
+    # convert from [x,y,w,h,score,class] to [score,class,x,y,w,h]
+    pred = pred[:, [4, 5, 0, 1, 2, 3]]
+    return pred
+    
 
 def main():    
-
-    data_path = '../Datasets/PASCAL_VOC/'
+    data_path = '../Datasets/MS-COCO/'
     
+    image_size = 640
     
-
+    # Class labels 
+    classes = [
+        'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck',
+        'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 
+        'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra',
+        'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee',
+        'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove',
+        'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+        'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 
+        'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 
+        'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 
+        'remote', 'keyboard','cell phone', 'microwave', 'oven', 'toaster', 'sink', 
+        'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 
+        'hair drier', 'toothbrush'
+    ]
+    class_labels = BidirectionalMap()
+    for i in range(len(classes)):
+        class_labels.add_mapping(i, classes[i])
+    
+    # Save the instance to a pickle file
+    with open("class_ind_pair.pkl", "wb") as f:
+        pickle.dump(class_labels, f)
+    
     # Creating train dataset object 
-    dataset = Dataset( 
-        csv_file = data_path + "train.csv", 
-        image_dir = data_path + "images/", 
-        label_dir = data_path + "labels/", 
-        grid_sizes=grid_cell_sizes, 
-        anchors=ANCHORS, 
-        transform=aug_transforms 
+    train_dataset = YOLO_Dataset( 
+        image_dir = data_path + "train2017/",
+        label_dir = data_path + "annotations_trainval2017/instances_train2017.json",
+        class_labels_map = class_labels,
+        augment = True,
+        input_size = image_size
+    )
+    
+    # Creating valid dataset object
+    valid_dataset = YOLO_Dataset( 
+        image_dir = data_path + "val2017/",
+        label_dir = data_path + "annotations_trainval2017/instances_val2017.json", 
+        class_labels_map = class_labels,
+        augment = False,
+        input_size = image_size
+    )
+    
+    
+    # visualize some train examples (with augmentation)
+    for i in range(0, len(train_dataset), len(train_dataset)//6):
+        # extract x, y
+        image, target = train_dataset[i]
+        
+        # Plotting the image with the bounding boxes 
+        plot_image(image.permute(1,2,0), target, class_labels)
+        
+    
+    # visualize some valid examples (without augmentation)
+    for i in range(0, len(valid_dataset), len(valid_dataset)//6):
+        # extract x, y
+        image, target = valid_dataset[i]
+        
+        # Plotting the image with the bounding boxes 
+        plot_image(image.permute(1,2,0), target, class_labels)
+    
+    
+    # Create data loaders for training and validation sets
+    train_loader = DataLoader(
+        train_dataset, batch_size=16, num_workers=4, pin_memory=True,
+        persistent_workers=True, shuffle=True, collate_fn=YOLO_Dataset.collate_fn
     )
 
-    
-    # visualize examples
-    for i in range(5):
-        # extract x, y
-        x, y = dataset[i]
-        
-        # unsqueeze the batch dim
-        x = x.unsqueeze(0)
-        y = [item.unsqueeze(0) for item in y]
-
-        # Getting the boxes coordinates from the labels 
-        # and converting them into bounding boxes without scaling 
-        boxes = [] 
-        for j in range(y[0].shape[1]): 
-            boxes += convert_cells_to_bboxes(y[j], scaled_anchors[j], is_predictions=False)[0] 
-        
-        # Applying non-maximum suppression 
-        boxes = nms(boxes, iou_threshold=1, confidence_threshold=0.7) 
-        
-        # Plotting the image with the bounding boxes 
-        plot_image(x[0].permute(1,2,0).to("cpu"), boxes, class_labels)
-
-
-    # train/valid split
-    val_size = int(0.1 * len(dataset))
-    train_size = len(dataset) - val_size
-    
-    # Split the dataset into training and validation sets
-    random_seed = 42 # Set random seed for reproducibility
-    torch.manual_seed(random_seed)
-    random.seed(random_seed)
-    train_dataset, val_dataset = random_split(dataset, [train_size, val_size], generator=torch.Generator().manual_seed(random_seed))
-
-    # Create data loaders for training and validation sets
-    train_loader = DataLoader(train_dataset, batch_size=8, num_workers=4, shuffle=True, pin_memory=True)
-    val_loader = DataLoader(val_dataset, batch_size=40, num_workers=5, pin_memory=True)  
+    val_loader = DataLoader(
+        valid_dataset, batch_size=32, num_workers=4, pin_memory=True,
+        persistent_workers=True, shuffle=False, collate_fn=YOLO_Dataset.collate_fn
+    )  
     
     # create model 
-    model = YOLOv3(num_classes=len(class_labels)).to(GPU_Device()) 
-    if f'{type(model).__name__}.pth' in os.listdir():
-        model.load_state_dict(torch.load(f'{type(model).__name__}.pth'))
-        
+    model = YOLO(size='s', num_classes=len(class_labels))
+    model = model.to(compute_device())
+    
     # model training
-    model_training(train_loader, val_loader, model, YOLOLoss(), scaled_anchors.to(GPU_Device()), len(class_labels))
+    model_training(train_loader, val_loader, model)
     
     # load the best model
-    model.load_state_dict(torch.load(f'{type(model).__name__}.pth'))
-    
-    
-    # test dataset
-    test_dataset = Dataset( 
-        csv_file = data_path+"test.csv", 
-        image_dir = data_path+"images/", 
-        label_dir = data_path+"labels/", 
-        anchors = ANCHORS, 
-        transform = resize_transform 
-    ) 
-    print('length dataset', len(test_dataset))
-    test_loader = DataLoader(test_dataset, batch_size=40, num_workers=5, pin_memory=True)
-    loss, mAP = feedforward(test_loader, model, YOLOLoss(), scaled_anchors.to(GPU_Device()), len(class_labels))
-    print(f"Test mAP: {mAP:.3f} | Test Loss: {loss:.3f}")
-    
-    # visualize test examples
-    for i in range(6):
-        # extract x
-        x, _ = test_dataset[i]
+    model.load_state_dict(torch.load(model.name() + '.pth'))
+
+    # inference using validation dataset
+    for i in range(0, len(valid_dataset), len(valid_dataset)//6):
+        # extract x, y
+        image, target = valid_dataset[i]
         
-        # move x to device
-        x = x.to(GPU_Device())
-        
-        # inference
-        boxes = inference(x, model)
+        # inferece
+        pred = inference(model, image)
         
         # Plotting the image with the bounding boxes 
-        plot_image(x.permute(1,2,0).detach().cpu(), boxes, class_labels)
-    
+        plot_images(image.permute(1,2,0), target, pred, class_labels)
+        
+        
     
 if __name__ == "__main__": 
     main()
